34c2cbc6c6360bb9eaf5f6e43e1f8bbb759f9806
Michael Scott <84039563+thebluenowhere@users.noreply.github.com>
2022-03-07 14:14:01 +0000

    Update LittleSistersEssay.py

--- LittleSistersEssay.py
+++ LittleSistersEssay.py
@@ -20,7 +20,7 @@ def clean_up_spacing(sentence):
     :param sentence: str a sentence to clean of leading and trailing space characters.
     :return: str a sentence that has been cleaned of leading and trailing space characters.
     """
-    return sentence. strip()
+    return sentence.strip()
 
 def replace_word_choice(sentence, old_word, new_word):
     """
